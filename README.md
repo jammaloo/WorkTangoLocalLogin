47c3bd978cd1b0d7f6f649d94c0137b9de2b4cd4
James Freeman <james.freeman@worktango.com>
2023-05-26 10:56:30 -0400

    feat: add login script with install links

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # WorkTangoLocalLogin
 Add quick links to remove/set the kwref on the login page
+
+## Installation
+Make sure you have user scripts enabled in your browser (these instructions refer to the latest versions of the browser):
+
+* Firefox - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=firefox).
+* Chrome - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=chrome).
+* Opera - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=opera).
+* Safari - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=safari).
+* Dolphin - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=dolphin).
+* UC Browser - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=ucweb).
+
+### Install scripts
+
+[Download the latest version](https://raw.githubusercontent.com/jammaloo/WorkTangoLocalLogin/main/WorkTangoLocalLogin.user.js)
---
+++ WorkTangoLocalLogin.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         WorkTango Local Login
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Add quick links to set or remove the kwref on login
+// @author       You
+// @namespace   https://github.com/jammaloo
+// @updateURL  https://raw.githubusercontent.com/jammaloo/WorkTangoLocalLogin/main/WorkTangoLocalLogin.user.js
+// @downloadURL  https://raw.githubusercontent.com/jammaloo/WorkTangoLocalLogin/main/WorkTangoLocalLogin.user.js
+// @match        https://*.rr.develop.non-prod.kazoohr.io/users/sign_in?kwref=*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=worktango.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const local_kwref = 'http://localhost:8888/';
+
+    const loginContainer = document.querySelector('.login-form');
+    if (!loginContainer) {
+        console.warn('Local Login Tamperscript could not find login form');
+    }
+    const pageUrl = new URL(document.location.href);
+	const kwref = pageUrl.searchParams.get('kwref');
+
+    const noKwref = new URL(pageUrl);
+    noKwref.searchParams.set('kwref', '');
+    const localKwref = new URL(pageUrl);
+    localKwref.searchParams.set('kwref', local_kwref);
+
+
+    const url_id = 'tampermonkey_site_url';
+    const remove_id = 'tampermonkey_remove_id';
+    const set_local_id = 'tampermonkey_set_local_id';
+    const htmlTemplate = `
+    <div>
+    	<h2 style="display: ${kwref ? 'block' : 'none'}">Logging in to <span id="${url_id}"></span></h2>
+
+        <h2 style="display: ${kwref ? 'none' : 'block'}">No kwref set</span></h2>
+
+        <a style="display: ${kwref ? 'block' : 'none'}" href="#" id="${remove_id}">Remove kwref</a>
+
+        <a style="display: ${kwref !== local_kwref ? 'block' : 'none'}" href="#" id="${set_local_id}">Set kwref to ${local_kwref}</a>
+    </div>`;
+	const domOutput = new DOMParser().parseFromString(htmlTemplate, "text/html");
+    domOutput.getElementById(url_id).textContent = kwref;
+    domOutput.getElementById(remove_id).setAttribute('href', noKwref);
+    domOutput.getElementById(set_local_id).setAttribute('href', localKwref);
+    loginContainer.prepend(domOutput.body.firstChild);
+})();
